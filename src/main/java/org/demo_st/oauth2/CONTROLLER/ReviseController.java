e8895ed3fd7345c303fd2803049f8e2124cd185c
Kutakula Sasank <kutakula.sasank2022@vitstudent.ac.in>
2026-01-05 09:39:02 +0530

    modificatoin in reise controller

--- src/main/java/org/demo_st/oauth2/CONTROLLER/ReviseController.java
+++ src/main/java/org/demo_st/oauth2/CONTROLLER/ReviseController.java
@@ -8,6 +8,7 @@ import org.demo_st.oauth2.Service.Service1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,73 +22,45 @@ public class ReviseController {
     @Autowired
     private Validatetoken validatetoken;
 
-    @GetMapping("/topics/{topic}")  //GET SIDE BAR WORK TOPIC
-    public ResponseEntity<?> topics(@PathVariable String topic, @RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        List<Mytopcs> topics = service1.gettopics(topic,validatetoken.extractEmail(authheader.substring(7)));
-        return new ResponseEntity(topics, HttpStatus.OK);
+    @GetMapping("/topics/{topic}")
+    public ResponseEntity<?> topics(@PathVariable String topic, Authentication auth) {
+        return ResponseEntity.ok(service1.gettopics(topic, auth.getName()));
     }
 
     @GetMapping("/questions") //USED TO FETCH QUESTIONS BASED ON EMAIL -> NOT USED DELETE IT
-    public ResponseEntity<?> questions(@RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        List<Questions> questions = service1.getquestions(validatetoken.extractEmail(authheader.substring(7)));
-        return new ResponseEntity(questions,HttpStatus.OK);
+    public ResponseEntity<?> questions(Authentication auth){
+        return new ResponseEntity<>(service1.getquestions(auth.getName()),HttpStatus.OK);
     }
 
     @PostMapping("/adddsawork") // ONLY THING TO ADD DSA WORK
-    public ResponseEntity<?> add_dsa_work(@RequestBody Mywork mywork, @RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        mywork.setEmail(validatetoken.extractEmail(authheader.substring(7)));
+    public ResponseEntity<?> add_dsa_work(@RequestBody Mywork mywork, Authentication auth){
+        mywork.setEmail(auth.getName());
         return service1.add_dsa_work(mywork);
     }
 
     @PostMapping("/addotherwork") //TO ADD OTHER WORK NOT DSA
-    public ResponseEntity<?> add_other_work(@RequestBody Mywork mywork,@RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        mywork.setEmail(validatetoken.extractEmail(authheader.substring(7)));
+    public ResponseEntity<?> add_other_work(@RequestBody Mywork mywork,Authentication  auth){
+        mywork.setEmail(auth.getName());
         return service1.add_other_work(mywork);
     }
 
     @GetMapping("/getdsawork") // MAIN FUNCTION TO GET ONLY DSA QUESTIONS
-    public ResponseEntity<?> get_dsa_work(@RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        return service1.get_dsa_work(validatetoken.extractEmail(authheader.substring(7)));
+    public ResponseEntity<?> get_dsa_work(Authentication auth){
+        return service1.get_dsa_work(auth.getName());
     }
 
     @GetMapping("/getwork/{topic}") //GET WORK OTHER THAN DSA BASED ON TOPIC
-    public ResponseEntity<?> get_other_work(@PathVariable String topic,@RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        return service1.get_other_work(validatetoken.extractEmail(authheader.substring(7)),topic);
+    public ResponseEntity<?> get_other_work(@PathVariable String topic,Authentication auth){
+        return service1.get_other_work(auth.getName(),topic);
     }
 
-//    @GetMapping("/toggleattempt")
-
     @GetMapping("/toglleattempt/{id}")
-    public ResponseEntity<?> toglle_attempt(@PathVariable Long id,@RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        return service1.toggleattempt(validatetoken.extractEmail(authheader.substring(7)),id);
+    public ResponseEntity<?> toglle_attempt(@PathVariable Long id,Authentication auth){
+        return service1.toggleattempt(auth.getName(), id);
     }
 
     @PostMapping("/modify") // TP MODIFY THE THING
-    public ResponseEntity<?> modify(@RequestBody Mywork mywork,@RequestHeader("Authorization") String authheader){
-        if(!validatetoken.validate(authheader.substring(7))){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("session expired please try again");
-        }
-        return service1.modifydata(validatetoken.extractEmail(authheader.substring(7)),mywork);
+    public ResponseEntity<?> modify(@RequestBody Mywork mywork,Authentication auth){
+        return service1.modifydata(auth.getName() , mywork);
     }
 }
